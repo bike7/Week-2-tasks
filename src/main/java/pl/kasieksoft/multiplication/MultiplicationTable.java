906fcce65a22ea864e8a7797ed6501080780942a
pytel <pytel.kat@gmail.com>
2018-11-06 20:57:19 +0100

    implement multiplication table

---
+++ src/main/java/pl/kasieksoft/multiplication/MultiplicationTable.java
@@ -0,0 +1,24 @@
+package pl.kasieksoft.multiplication;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        printMultiplicationTable(17);
+    }
+
+    private static void printMultiplicationTable(int size) {
+        System.out.print("      ");
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%4d", i);
+        }
+        System.out.println();
+        System.out.println();
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%4d", i);
+            System.out.print("  ");
+            for (int j = 1; j <= size; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
