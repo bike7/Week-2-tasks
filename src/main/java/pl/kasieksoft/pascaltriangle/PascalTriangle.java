b87d247fec9d828414634298bee1c9b329d87bfb
pytel <pytel.kat@gmail.com>
2018-11-07 15:56:56 +0100

    PascalTriangle corrected

--- src/main/java/pl/kasieksoft/pascaltriangle/PascalTriangle.java
+++ src/main/java/pl/kasieksoft/pascaltriangle/PascalTriangle.java
@@ -3,16 +3,16 @@ package pl.kasieksoft.pascaltriangle;
 public class PascalTriangle {
 
     public static void main(String[] args) {
-        printPascalTriangle(10);
+        printPascalTriangle(20);
     }
 
     private static void printPascalTriangle(int rows) {
         for (int i = 0; i < rows; i++) {
 
-            printSpace((rows - i) * 2);
+            printSpaces((rows - i) * 3);
 
             for (int j = 0; j <= i; j++) {
-                System.out.printf("%4d", pascalValue(i, j));
+                System.out.printf("%6d", pascalValue(i, j));
             }
 
             System.out.println();
@@ -20,16 +20,10 @@ public class PascalTriangle {
     }
 
     private static int pascalValue(int i, int j) {
-        if (j == 0) {
-            return 1;
-        } else if (j == i) {
-            return 1;
-        } else {
-            return pascalValue(i - 1, j - 1) + pascalValue(i - 1, j);
-        }
+        return (j == 0 || j == i) ? 1 : pascalValue(i - 1, j - 1) + pascalValue(i - 1, j);
     }
 
-    private static void printSpace(int count) {
+    private static void printSpaces(int count) {
         for (int i = 0; i < count; i++) {
             System.out.print(' ');
         }
