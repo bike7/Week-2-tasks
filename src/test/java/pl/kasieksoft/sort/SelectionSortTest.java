02ec63dbb749aa7382811de62a67687fe834e91b
Katarzyna Pytel <pytel.kat@gmail.com>
2018-11-12 15:56:58 +0100

    change sortingMethod to private

--- src/test/java/pl/kasieksoft/sort/SelectionSortTest.java
+++ src/test/java/pl/kasieksoft/sort/SelectionSortTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SelectionSortTest {
 
-    SortingMethod sortingMethod;
+    private SortingMethod sortingMethod;
 
     @BeforeEach
     void setup() {
